c23f2ad05528f0374ec5a15417e535b27f811503
GalNaveh <103554156+GalNaveh@users.noreply.github.com>
2022-06-12 19:38:11 +0800

    test

--- Good_sunset/app.py
+++ Good_sunset/app.py
@@ -136,7 +136,7 @@ def condition_calc(conditions):
 # ok = Image.open(r'C:\Users\galna\OneDrive\Documents\Good_sunset\ok.jpg')
 # trash = Image.open(r'C:\Users\galna\OneDrive\Documents\Good_sunset\bad.jpg')
 
-amazing = Image.open(r'sunset\amazing.jpg')
+amazing = Image.open('sunset\amazing.jpg')
 good = Image.open(r'https://github.com/GalNaveh/sunset/blob/main/Good_sunset/good.jpg','rb')
 ok = Image.open(r'https://github.com/GalNaveh/sunset/blob/main/Good_sunset/ok.jpg')
 trash = Image.open(r'https://github.com/GalNaveh/sunset/blob/main/Good_sunset/bad.jpg')
