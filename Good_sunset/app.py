9eff16ddf23a0c2c0af3839fc5d8f5a84e0eb208
GalNaveh <103554156+GalNaveh@users.noreply.github.com>
2022-06-12 19:11:00 +0800

    Update app.py

--- Good_sunset/app.py
+++ Good_sunset/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-from streamlit-lottie import st_lottie
+from streamlit_lottie import st_lottie
 from pydoc import visiblename
 from bs4 import BeautifulSoup
 from PIL import Image
